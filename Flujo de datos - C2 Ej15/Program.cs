b6790e43d21a8b6b366ee052d11beb56c0b348ad
Agneta <agnetagsu@yahoo.es>
2021-05-22 21:06:21 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.WriteLine("Introduce un numero:");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Es divisible por 2");
+            }
+            else
+            {
+                Console.WriteLine("No es divisible por 2");
+            }
+        }
+    }
+}
